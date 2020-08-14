eacefaaee3b418f2cede914a6bf3aeab3f9880a0
Thiago Tardioli <tmtardioli@icloud.com>
2020-08-14 14:32:04 +0200

    pseudo code

---
+++ adventure.py
@@ -0,0 +1,18 @@
+# As a game developer I want to have random
+# enemies so that a user won't be bored.
+
+# As a game developer I want to have a mistery
+# weapon so that a user can get excited with
+# different levels of dificulty.
+
+# As a user I want to have some dramatic pauses
+# during the game so that can be more exciting
+
+# As a game developer I want to have a option checker
+# so that we can prevent a user to select invalid options
+
+# As a game developer I want to have a fight with
+# enemies so that the game can be challenging
+
+# As a game developer I want to have an option to
+# escape the enemy so that a user won't die (game over)
